3660b519e3c786aee750cb413bad5935327f5006
AeDudics <142440963+AeDudics@users.noreply.github.com>
2024-01-26 21:45:56 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -43,14 +43,17 @@ public class PigLatin {
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-		     if(findFirstVowel(sWord) == -1) {
-		    	return sWord + "ay";
-	            }else if(findFirstVowel(sWord) == 0){
-		    	return sWord + "way";
-	            }else if(sWord.substring(0,2).equals("qu")){
-		    	return sWord.substring(2) + "quay";
-	            }else {
-		    	return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
-	    	    }
+	if(findFirstVowel(sWord) == -1)
+  
+    		return sWord + "ay";
+  
+ 	else if(findFirstVowel(sWord) == 0)
+  
+    		return sWord + "way";
+   	else if(sWord.substring(0,2).equals("qu"))
+  
+    		return sWord.substring(2) + "quay";
+   	else 
+    		return sWord.substring(findFirstVowel(sWord)) + sWord.substring(0,findFirstVowel(sWord)) + "ay";
     }
 }//end PigLatin class
