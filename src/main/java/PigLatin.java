9801c67628ccc4ea13d159891c7b227e43e05bce
AeDudics <142440963+AeDudics@users.noreply.github.com>
2024-01-26 21:15:02 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -50,8 +50,8 @@ public class PigLatin {
 	    }else if(sWord.substring(0,2) == "qu"){
 		    return sWord.substring(2) + "quay";
 	    }else if(findFirstVowel(sWord) > 0){
-		    return sWord.substring(1) + sWord.substring(0,1) + "ay";
 		    break;
+		    return sWord.substring(1) + sWord.substring(0,1) + "ay";
 	    }else{
 		    return "ERROR!";
 	    }
